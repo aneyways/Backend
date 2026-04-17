32c532e451d3e0e06182b3090a616818d64ac610
Ana Doibani <ana.doibani@icloud.com>
2026-04-17 23:07:51 +0300

    Add project files.

---
+++ AudioShop.Api/Controller/UserController.cs
@@ -0,0 +1,23 @@
+﻿using AudioShop.Api.Domain; 
+using Microsoft.AspNetCore.Http;    
+using Microsoft.AspNetCore.Mvc; 
+
+
+namespace AudioShop.Api.Controller
+{ 
+    [Route("api/user")] 
+    [ApiController] 
+
+
+    public class UserController : ControllerBase    
+    {
+        public static List<User> _users = new();  
+        public static int NextId = 1;
+
+        [HttpGet("all")] 
+        public IActionResult GetAllUsers() 
+        {
+            return Ok(_users);
+        }   
+    }
+}
