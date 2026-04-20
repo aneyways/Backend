470a62d543040a4cd243c214870f9d7759cc2a57
Ana Doibani <ana.doibani@icloud.com>
2026-04-20 22:16:19 +0300

    add CRUD

--- AudioShop.Api/Controller/UserController.cs
+++ AudioShop.Api/Controller/UserController.cs
@@ -31,6 +31,43 @@ namespace AudioShop.Api.Controller
             return Ok(user);
         }
 
+        [HttpPost("create")]    
+        public IActionResult CreateUser([FromBody] User user) 
+        {
+          
+            user.Id = NextId++;
+            user.CreatedAt = DateTime.UtcNow;
+            _users.Add(user);
+            return Created( $"/api/user/{user.Id}", user );
+        }   
+
+            [HttpPut("{id}")]    
+           public IActionResult UpdateUser(int id, [FromBody] User updatedUser)
+        {   
+            var existinguser = _users.FirstOrDefault(u => u.Id == id);
+            
+            if (existinguser == null) 
+            {
+                return NotFound(new { Message = $"User with {id} not found" });
+            }
+            existinguser.Username = updatedUser.Username;
+            existinguser.Email = updatedUser.Email;
+            
+            return Ok(existinguser);
+            }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id) 
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null) 
+            {
+                return NotFound(new { Message = $"User with {id} not found" });
+            }
+            _users.Remove(user);
+            return NoContent();
+        }
+
 
     }
 }
