4ef8ed1947da60e590329df79f6d4ffbb522bb25
Ana Doibani <ana.doibani@icloud.com>
2026-04-20 21:54:18 +0300

    add get user by id

--- AudioShop.Api/Controller/UserController.cs
+++ AudioShop.Api/Controller/UserController.cs
@@ -18,6 +18,19 @@ namespace AudioShop.Api.Controller
         public IActionResult GetAllUsers() 
         {
             return Ok(_users);
-        }   
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id) 
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null) 
+            {
+                return NotFound(new { Message = $"User with {id} not found" });
+            }
+            return Ok(user);
+        }
+
+
     }
 }
