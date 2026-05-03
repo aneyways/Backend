157b60a0988f7adac76f5c08cbbe65366e7ae8c5
Ana Doibani <ana.doibani@icloud.com>
2026-05-03 15:30:33 +0300

    add businessl logic

---
+++ AudioShop.BusinessLogic/Interface/BussinesLogic.cs
@@ -0,0 +1,14 @@
+﻿
+using AudioShop.BusinessLogic.Interface;
+
+
+namespace AudioShop.BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public UserBL GetUserBL()
+        {
+            return new UserBL();
+        }
+    }
+}
