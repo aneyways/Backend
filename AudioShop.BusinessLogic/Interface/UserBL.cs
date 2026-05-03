39b9470907f601b553bddc0d02b918b67ee55a08
Ana Doibani <ana.doibani@icloud.com>
2026-05-03 16:00:53 +0300

    add bussines logic

--- AudioShop.BusinessLogic/Interface/UserBL.cs
+++ AudioShop.BusinessLogic/Interface/UserBL.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace AudioShop.BusinessLogic.Interface
 {
-    public class UserBL : IUser 
+    public class UserBL : IUser
     {
     }
 }
