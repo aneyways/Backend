992b201fe714320e107a0b451c2d5f089b0db976
anna <annaceban04@gmail.com>
2026-05-15 03:24:39 +0300

    Changed Domains entities

--- AudioShop.Domain/Entitiies/Order/OrderData.cs
+++ AudioShop.Domain/Entitiies/Order/OrderData.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AudioShop.Domains.Entitiies.Order;
+using AudioShop.Domains.Entitiies.User;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using AudioShop.Domains.Entitiies.Order;
 
 namespace AudioShop.Domains.Entitiies.Order
 {
@@ -10,16 +11,18 @@ namespace AudioShop.Domains.Entitiies.Order
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
         public int UserId { get; set; }
 
+        [ForeignKey("UserId")]
+        public UserData? User { get; set; }
 
         [InverseProperty("Order")]
-        public List<OrderItemData> Items { get; set; }
+        public List<OrderItemData> Items { get; set; } = new();
 
         public decimal Total { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime Created { get; set; }
-
         public bool IsDeleted { get; set; }
     }
 }
